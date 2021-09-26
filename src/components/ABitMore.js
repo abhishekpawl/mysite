6d3014964fce654e9a4cd2530f95798367d47dcb
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2021-09-26 21:55:06 +0530

    set up aBitMore page

--- src/components/ABitMore.js
+++ src/components/ABitMore.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {FaRegQuestionCircle} from 'react-icons/fa';
 
 const ABitMore = () => {
   return (
-    <h2>
-      a bit more
-    </h2>
+    <section className="section">
+      <div className="title">
+        <h2>
+        <span><FaRegQuestionCircle /></span>a bit more about me
+        </h2>
+      </div>
+    </section>
   )
 }
 
