a5fbf1c7891380d2173f33f5c907cd700944dd9d
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2021-10-10 10:17:12 +0530

    removed background

--- src/App.js
+++ src/App.js
@@ -19,37 +19,38 @@ function App() {
 
   return(
     <React.Fragment>
-    {
-      loading ? (
-        <div className="loader">
-          <BounceLoader size={100} color={'#4D7919'} loading={loading}></BounceLoader>
-        </div>
-      ) : (
-        <Router>
-          <Navbar></Navbar>
-          <Switch>
-            <Route exact path="/">
-              <AboutMe />
-            </Route>
-            <Route path='/education'>
-              <Education />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/aBitMore">
-              <ABitMore />
-            </Route>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="*">
-              <AboutMe />
-            </Route>
-          </Switch>
-        </Router>
-      )
-    }
+      {
+        loading ? (
+          <div className="loader">
+            <BounceLoader size={100} color={'#4D7919'} loading={loading}></BounceLoader>
+          </div>
+        ) : (
+          <Router>
+            <Navbar></Navbar>
+            <Background className="tsparticle" />
+            <Switch>
+              <Route exact path="/">
+                <AboutMe />
+              </Route>
+              <Route path='/education'>
+                <Education />
+              </Route>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/aBitMore">
+                <ABitMore />
+              </Route>
+              <Route path="/contact">
+                <Contact />
+              </Route>
+              <Route path="*">
+                <AboutMe />
+              </Route>
+            </Switch>
+          </Router>
+        )
+      }
     </React.Fragment>
   )
 }
