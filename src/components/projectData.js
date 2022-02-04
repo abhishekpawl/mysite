730a7a1578bc7ac7e7b45f50605541f47ac220b8
Abhishek Paul <abhishek.paulcp.dbs@gmail.com>
2022-02-04 11:29:28 +0530

    Updated projectData.js

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,46 +1,55 @@
-export const projectData = [
-  {
-    id: 1,
-    title: 'Gait Recognition in Occlusion and Human Re-Identificaiton',
-    tech: [
-      'Machine Learning',
-      'Image Processing',
-      'Python',
-      'Tensorflow',
-      'NumPy'
-    ],
-    text: 'Designed and built a Machine Learning pipeline to effectivelly reconstruct occluded sequences from incomplete Gait Cycles for efficient offline Gait Recognition and identification of individuals.',
-    url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1632648854/pipe_jneq33.gif',
-    link: '#'
-  },
-  {
-    id: 2,
-    title: 'Stick Notes/Cards',
-    tech: [
-      'HTML', 'CSS', 'JavaScript', 'React', 'Node.js'
-    ],
-    text: 'A static app made with the use of React.js to use as stick notes.',
-    url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1632648854/note_syo4hw.gif',
-    link: 'https://paul-sticknotes.netlify.app/'
-  },
-  {
-    id: 3,
-    title: 'Co-Codepen',
-    tech: [
-      'HTML', 'CSS', 'JavaScript', 'Node.js', 'socket.io'
-    ],
-    text: 'A collaborative codepen made realtime with the help of socket.io package.',
-    url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1640611046/cocodepen_krgy3l.gif',
-    link: 'https://github.com/abhishekpawl/cocodepen'
-  },
-  {
-    id: 4,
-    title: 'Battle of Neighborhoods',
-    tech: [
-      'Machine Learning', 'APIs', 'Python'
-    ],
-    text: 'IBM Data Science Capstone Project: Devised a model using Foursquare API and Clustering to suggest tourists visiting Kolkata, India, places for recreational activities based on their location.',
-    url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1640612222/Battle-of-Neighborhoods_i9weo7.gif',
-    link: 'https://docs.google.com/presentation/d/1YVzhSuBM5gS2syceXWdN6RKBb8byGVm5/edit?usp=sharing&ouid=113707725644800956827&rtpof=true&sd=true'
-  }
+export const projectData = [{
+        id: 1,
+        title: 'Gait Recognition in Occlusion and Human Re-Identificaiton',
+        tech: [
+            'Machine Learning',
+            'Image Processing',
+            'Python',
+            'Tensorflow',
+            'NumPy'
+        ],
+        text: 'Designed and built a Machine Learning pipeline to effectivelly reconstruct occluded sequences from incomplete Gait Cycles for efficient offline Gait Recognition and identification of individuals.',
+        url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1632648854/pipe_jneq33.gif',
+        link: '#'
+    },
+    {
+        id: 2,
+        title: 'Stick Notes/Cards',
+        tech: [
+            'HTML', 'CSS', 'JavaScript', 'React', 'Node.js'
+        ],
+        text: 'A static app made with the use of React.js to use as stick notes.',
+        url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1632648854/note_syo4hw.gif',
+        link: 'https://paul-sticknotes.netlify.app/'
+    },
+    {
+        id: 3,
+        title: 'Co-Codepen',
+        tech: [
+            'HTML', 'CSS', 'JavaScript', 'Node.js', 'socket.io'
+        ],
+        text: 'A collaborative codepen made realtime with the help of socket.io package.',
+        url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1640611046/cocodepen_krgy3l.gif',
+        link: 'https://github.com/abhishekpawl/cocodepen'
+    },
+    {
+        id: 4,
+        title: 'Battle of Neighborhoods',
+        tech: [
+            'Machine Learning', 'APIs', 'Python'
+        ],
+        text: 'IBM Data Science Capstone Project: Devised a model using Foursquare API and Clustering to suggest tourists visiting Kolkata, India, places for recreational activities based on their location.',
+        url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1640612222/Battle-of-Neighborhoods_i9weo7.gif',
+        link: 'https://docs.google.com/presentation/d/1YVzhSuBM5gS2syceXWdN6RKBb8byGVm5/edit?usp=sharing&ouid=113707725644800956827&rtpof=true&sd=true'
+    },
+    {
+        id: 5,
+        title: 'Pomodoro App',
+        tech: [
+            'HTML', 'CSS', 'JavaScript'
+        ],
+        text: 'Pomodoro methodology to improve productivity',
+        url: 'https://res.cloudinary.com/dyiwzykis/image/upload/v1643953071/pomodoro_ckdlfz.gif',
+        link: 'https://pomodoro-app-k5bprdhxo-abhishekpawl.vercel.app/'
+    }
 ]
